e1bf02a1a33bef5d2823f9fae539313a6c444218
葉永青 Chad Yeh <chad.yeh@miniasp.com>
2020-08-22 16:04:39 +0800

    設定 routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,22 +4,31 @@ import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ColorComponent } from './utilities/color/color.component';
+import { LayoutComponent } from './layout/layout.component';
+import { LoginComponent } from './login/login.component';
 // import { PathNotFoundComponent } from './path-not-found/path-not-found.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'page1', component: Page1Component },
-  { path: 'page2', component: Page2Component },
   {
-    path: 'utilities',
+    path: '',
+    component: LayoutComponent,
     children: [
-      { path: 'color', component: ColorComponent },
-      { path: 'color/:type', component: ColorComponent },
+      { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'page1', component: Page1Component },
+      { path: 'page2', component: Page2Component },
+      {
+        path: 'utilities',
+        children: [
+          { path: 'color', component: ColorComponent },
+          { path: 'color/:type', component: ColorComponent },
+        ]
+      },
+      { path: 'pages', loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule) },
     ]
   },
-  { path: 'pages', loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule) },
-  // { path: '**', component: PathNotFoundComponent }
+  { path: 'login', component: LoginComponent },
+  // { path: '**', component: PathNotFoundComponent },
 ];
 
 @NgModule({
